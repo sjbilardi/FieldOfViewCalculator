1147fc508a40126c4d355727fbcbc956607a14df
Sergei Bilardi <bilardis@my.erau.edu>
2017-04-04 16:23:23 -0400

    Add files via upload

--- FOVExample.py
+++ FOVExample.py
@@ -1,10 +1,10 @@
 import FOVCalculator as fov
 
 # Distances
-avg_plane_alt = 35000 * 0.3048/1000		# km (average)
+avg_plane_alt = 35000 * 0.3048/1000			# kilometers (average)
 
 # Objects
-boing737 = ['Boing 737-100', 28.65, 28.35] # [name, height, width] (Boing)
+boing737 = ['Boing 737-100', 28.65, 28.35] 	# meters [name, height, width] (Boing)
 
 # Manta, C14 Optical System (Boing 737)
 manta = fov.Camera(name='Allied Vision Manta G-235', pixSize=5.86, sensorSize=[1936, 1216])
